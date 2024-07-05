d8327543f094f17b3c213516cf7984a5094f28ac
Ankit Singh <ankit.singh@convesio.com>
2024-07-05 14:03:00 +0530

    dynamic method GET, POST etc

--- server.js
+++ server.js
@@ -8,63 +8,114 @@ async function y() {
     let x;
     // ----------------------- correct apis ---------------------
 
-    // POST /posts    
+    // PATCH
     // x = await apiRetry(
-    //     "POST",
-    //     "https://jsonplaceholder.typicode.com/posts/1",
-    //     {
-    //         "Content-type": "application/json; charset=UTF-8",
-    //     },
-    //     {
-    //         title: "foo",
-    //         body: "bar",
-    //         userId: 1,
-    //     }
+    //   "PATCH",
+    //   "https://jsonplaceholder.typicode.com/posts/1",
+    //   { "Content-Type": "application/json" },
+    //   {
+    //     title: "foo"
+    //   }
     // );
 
-    // GET /posts
+    // PUT
+    // x = await apiRetry(
+    //   "PUT",
+    //   "https://jsonplaceholder.typicode.com/posts/1",
+    //   { "Content-Type": "application/json" },
+    //   {
+    //     id: 1,
+    //     title: "foo",
+    //     body: "bar",
+    //     userId: 1,
+    //   }
+    // );
+
+    // POST
+    // x = await apiRetry(
+    //   "POST",
+    //   "https://jsonplaceholder.typicode.com/posts",
+    //   { "Content-Type": "application/json" },
+    //   {
+    //     title: "foo",
+    //     body: "bar",
+    //     userId: 1,
+    //   }
+    // );
+
+    // GET
     // x = await apiRetry("GET", "https://jsonplaceholder.typicode.com/posts");
 
     // GET /posts/1
-    x = await apiRetry("GET", "https://jsonplaceholder.typicode.com/posts/1");
+    // x = await apiRetry("GET", "https://jsonplaceholder.typicode.com/posts/1");
+
+    // GET /posts?userId=1
+    // x = await apiRetry("GET", "https://jsonplaceholder.typicode.com/posts?userId=1");
+
+    // DELETE
+    // x = await apiRetry("DELETE", "https://jsonplaceholder.typicode.com/posts/1");
 
 
     // ----------------------- incorrect apis ---------------------
 
-    // POST /posts    
+    // PATCH
     // x = await apiRetry(
-    //     "POST",
-    //     "https://djsonplaceholder.typicode.com/posts/1",
-    //     {
-    //         "Content-type": "application/json; charset=UTF-8",
-    //     },
-    //     {
-    //         title: "foo",
-    //         body: "bar",
-    //         userId: 1,
-    //     }
+    //   "PATCH",
+    //   "https://djsonplaceholder.typicode.com/posts/1",
+    //   { "Content-Type": "application/json" },
+    //   {
+    //     title: "foo"
+    //   }
     // );
 
-    // GET /posts
+    // PUT
+    // x = await apiRetry(
+    //   "PUT",
+    //   "https://djsonplaceholder.typicode.com/posts/1",
+    //   { "Content-Type": "application/json" },
+    //   {
+    //     id: 1,
+    //     title: "foo",
+    //     body: "bar",
+    //     userId: 1,
+    //   }
+    // );
+
+    // POST
+    // x = await apiRetry(
+    //   "POST",
+    //   "https://djsonplaceholder.typicode.com/posts",
+    //   { "Content-Type": "application/json" },
+    //   {
+    //     title: "foo",
+    //     body: "bar",
+    //     userId: 1,
+    //   }
+    // );
+
+    // GET
     // x = await apiRetry("GET", "https://djsonplaceholder.typicode.com/posts");
 
     // GET /posts/1
     // x = await apiRetry("GET", "https://djsonplaceholder.typicode.com/posts/1");
-    // x = await apiRetry("GET", "https://cluster.qa8.cloud.conves.io:5443/env/1a5/apps/stacks");
 
+    // GET /posts?userId=1
+    // x = await apiRetry("GET", "https://djsonplaceholder.typicode.com/posts?userId=1");
+
+    // DELETE
+    // x = await apiRetry("DELETE", "https://djsonplaceholder.typicode.com/posts/1");
+
+
+
+    // ----------------------- slow apis ---------------------
+    // x = await apiRetry("GET", "https://cluster.qa8.cloud.conves.io:5443/env/1a5/apps/stacks");
 
     console.log("x: ");
     console.log(x);
     process.exit(0);
   } catch (error) {
-    // console.error( error);
-    // if(error.custom_error_message == "Could not connect to external api even after after trying 3 times" )
-    //     {
-    //         console.error("We need to stop this job and notify in slack !")
-    //     }
-    console.error("error");
+    // console.error("final error");
     console.error("We need to stop this job and notify in slack !");
-    
   }
 }
 
